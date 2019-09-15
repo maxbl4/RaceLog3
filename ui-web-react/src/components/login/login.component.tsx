daf4d345d69c207a77240562aa96d9e537ed1f99
komadmi <dmitriy.comarov@gmail.com>
2019-09-15 21:00:25 +0300

    Login component

--- ui-web-react/src/components/login/login.component.tsx
+++ ui-web-react/src/components/login/login.component.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Tabs, Tab, Form, Button } from "react-bootstrap";
 
 export class LoginComponent extends React.Component {
   render() {
-    return <div>Login Component</div>;
+    return (
+      <Tabs defaultActiveKey="signIn" id="loginTabs">
+        <Tab eventKey="signIn" title="Войти">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Почта</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control type="password" placeholder="Введите пароль" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Дави на газ!
+            </Button>
+          </Form>
+        </Tab>
+        <Tab eventKey="register" title="Зарегистрироваться">
+          <span>Registration form</span>
+        </Tab>
+      </Tabs>
+    );
   }
 }
