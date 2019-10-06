4179885fabe9457c94158ca712a43bec510bd4e4
komadmi <dmitriy.comarov@gmail.com>
2019-10-06 21:36:14 +0300

    Use Formik and Yup libs for registration form

--- ui-web-react/src/components/user/user.login.panel.component.tsx
+++ ui-web-react/src/components/user/user.login.panel.component.tsx
@@ -1,180 +1,196 @@
 import React from "react";
-import { Form, Button, InputGroup } from "react-bootstrap";
 import { UserInfo } from "../../model/types/datatypes";
 import { INITIAL_USER_INFO } from "../../model/reducers/user.reducer";
 import {
   ClassCompetition,
   getClassCompetitionName
 } from "../../model/types/class-competition.model";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikActions,
+  FormikProps
+} from "formik";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Неверный формат почты")
+    .required("Введите пожалуйста почту"),
+  password: Yup.string()
+    .min(5, "Пароль должен быть не менее 5-ти символов")
+    .required("Введите пожалуйста пароль")
+});
+
+const registerSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Неверный формат почты")
+    .required("Введите пожалуйста почту"),
+  password: Yup.string()
+    .min(5, "Пароль должен быть не менее 5-ти символов")
+    .required("Введите пожалуйста пароль"),
+  name: Yup.string()
+    .min(2, "Имя должно быть не менее 2-х символов")
+    .required("Введите пожалуйста имя"),
+  bikeNumber: Yup.number()
+    .positive("Номер байка должен быть положительным")
+    .integer("Номер байка должен быть целым числом")
+    .required("Введите пожалуйста номер байка")
+});
+
+interface UserInfoValues {
+  name: string;
+  password: string;
+  email: string;
+  bikeNumber: number;
+  classCompetition: ClassCompetition;
+}
 
 export type UserLoginPanelComponentProps = {
   mode: "login" | "register";
   onSubmit: (userInfo: UserInfo) => void;
 };
 
-export type UserLoginPanelComponentState = {
-  name: string;
-  password: string;
-  email: string;
-  bikeNumber: number;
-  classCompetition: ClassCompetition;
-  validated: boolean;
-};
-
 export class UserLoginPanelComponent extends React.Component<
-  UserLoginPanelComponentProps,
-  UserLoginPanelComponentState
+  UserLoginPanelComponentProps
 > {
-  constructor(props: UserLoginPanelComponentProps) {
-    super(props);
-
-    this.state = {
-      name: INITIAL_USER_INFO.name,
-      password: INITIAL_USER_INFO.password,
-      email: INITIAL_USER_INFO.email,
-      bikeNumber: INITIAL_USER_INFO.bikeNumber,
-      classCompetition: INITIAL_USER_INFO.classCompetition,
-      validated: false
-    };
-  }
-
-  handleSubmitClick = (event: any) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    } else {
-      this.props.onSubmit({
-        ...INITIAL_USER_INFO,
-        name: this.state.name,
-        password: this.state.password,
-        email: this.state.email,
-        bikeNumber: this.state.bikeNumber,
-        classCompetition: this.state.classCompetition
-      });
-    }
-
-    this.setState({ validated: true });
-  };
+  isLoginMode = (): boolean => this.props.mode === "login";
 
   getControlID = (name: string): string => this.props.mode + "_" + name;
 
   render() {
     return (
-      <Form
-        noValidate
-        validated={this.state.validated}
-        onSubmit={this.handleSubmitClick}
-      >
-        <Form.Group controlId={this.getControlID("formEmail")}>
-          <Form.Label>Почта</Form.Label>
-          <InputGroup>
-            <Form.Control
-              type="email"
-              placeholder="name@example.com"
-              onChange={(event: any) =>
-                this.setState({
-                  email: event.currentTarget.value
-                })
-              }
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Почта не может быть пустой
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-        <Form.Group controlId={this.getControlID("formPassword")}>
-          <Form.Label>Пароль</Form.Label>
-          <InputGroup>
-            <Form.Control
-              type="password"
-              placeholder="Введите пароль"
-              onChange={(event: any) =>
-                this.setState({
-                  password: event.currentTarget.value
-                })
-              }
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Пароль не может быть пустым
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-        {this.props.mode === "register" && (
-          <>
-            <Form.Group controlId={this.getControlID("formName")}>
-              <Form.Label>Имя</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  type="text"
-                  placeholder="Введите имя"
-                  onChange={(event: any) =>
-                    this.setState({
-                      name: event.currentTarget.value
-                    })
-                  }
-                  required
+      <div className="row col-lg-12">
+        <Formik
+          initialValues={{
+            ...INITIAL_USER_INFO
+          }}
+          validationSchema={this.isLoginMode() ? loginSchema : registerSchema}
+          onSubmit={(
+            values: UserInfoValues,
+            actions: FormikActions<UserInfoValues>
+          ) => {
+            this.props.onSubmit({
+              ...INITIAL_USER_INFO,
+              ...values
+            });
+            actions.setSubmitting(false);
+          }}
+          render={(formikBag: FormikProps<UserInfoValues>) => (
+            <Form>
+              <div className="form-group">
+                <label htmlFor="email">Почта</label>
+                <Field
+                  id={this.getControlID("email")}
+                  type="email"
+                  name="email"
+                  placeholder="Введите почту"
+                  className={`form-control ${
+                    formikBag.touched.email && formikBag.errors.email
+                      ? "is-invalid"
+                      : ""
+                  }`}
                 />
-                <Form.Control.Feedback type="invalid">
-                  Имя не может быть пустым
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
+                {this.renderError("email")}
+              </div>
 
-            <Form.Group controlId={this.getControlID("formBikeNumber")}>
-              <Form.Label>Номер байка</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  type="number"
-                  placeholder="Введите номер байка"
-                  onChange={(event: any) =>
-                    this.setState({
-                      bikeNumber: event.currentTarget.value
-                    })
-                  }
-                  required
+              <div className="form-group">
+                <label htmlFor="password">Пароль</label>
+                <Field
+                  id={this.getControlID("password")}
+                  type="password"
+                  name="password"
+                  placeholder="Введите пароль"
+                  className={`form-control ${
+                    formikBag.touched.password && formikBag.errors.password
+                      ? "is-invalid"
+                      : ""
+                  }`}
                 />
-                <Form.Control.Feedback type="invalid">
-                  Номер байка не может быть пустым
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
+                {this.renderError("password")}
+              </div>
 
-            <Form.Group controlId={this.getControlID("formClassCompetition")}>
-              <Form.Label>Класс соревнований</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  as="select"
-                  required
-                  onChange={(event: any) =>
-                    this.setState({
-                      classCompetition: event.currentTarget.value
-                    })
-                  }
-                >
-                  <option value="125cm3">
-                    {getClassCompetitionName("125cm3")}
-                  </option>
-                  <option value="250cm3">
-                    {getClassCompetitionName("250cm3")}
-                  </option>
-                  <option value="500cm3">
-                    {getClassCompetitionName("500cm3")}
-                  </option>
-                </Form.Control>
-                <Form.Control.Feedback type="invalid">
-                  Класс соревнований не может быть пустым
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </>
-        )}
-        <Button variant="primary" type="submit">
-          Дави на газ!
-        </Button>
-      </Form>
+              {!this.isLoginMode() && (
+                <React.Fragment>
+                  <div className="form-group">
+                    <label htmlFor="name">Имя</label>
+                    <Field
+                      id={this.getControlID("name")}
+                      type="text"
+                      name="name"
+                      placeholder="Введите имя"
+                      className={`form-control ${
+                        formikBag.touched.name && formikBag.errors.name
+                          ? "is-invalid"
+                          : ""
+                      }`}
+                    />
+                    {this.renderError("name")}
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="bikeNumber">Номер байка</label>
+                    <Field
+                      id={this.getControlID("bikeNumber")}
+                      type="number"
+                      name="bikeNumber"
+                      placeholder="Введите номер байка"
+                      className={`form-control ${
+                        formikBag.touched.bikeNumber &&
+                        formikBag.errors.bikeNumber
+                          ? "is-invalid"
+                          : ""
+                      }`}
+                    />
+                    {this.renderError("bikeNumber")}
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="classCompetition">Класс соревнований</label>
+                    <Field
+                      id={this.getControlID("classCompetition")}
+                      component="select"
+                      name="classCompetition"
+                      className="form-control"
+                    >
+                      <option value="125cm3">
+                        {getClassCompetitionName("125cm3")}
+                      </option>
+                      <option value="250cm3">
+                        {getClassCompetitionName("250cm3")}
+                      </option>
+                      <option value="500cm3">
+                        {getClassCompetitionName("500cm3")}
+                      </option>
+                    </Field>
+                  </div>
+                </React.Fragment>
+              )}
+              <button
+                type="submit"
+                className="btn btn-primary btn-block"
+                disabled={formikBag.isSubmitting}
+              >
+                {formikBag.isSubmitting
+                  ? "Идет прогрев мотора..."
+                  : "Дави на газ!!!"}
+              </button>
+            </Form>
+          )}
+        />
+      </div>
     );
   }
+
+  renderError = (fieldName: string): JSX.Element => {
+    return (
+      <ErrorMessage
+        component="div"
+        name={fieldName}
+        className="invalid-feedback"
+      />
+    );
+  };
 }
