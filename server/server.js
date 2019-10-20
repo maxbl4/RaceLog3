0778eee4efb476fd08dca9333777eb4fd5706fe4
komadmi <dmitriy.comarov@gmail.com>
2019-10-20 09:49:41 +0300

    Enhance server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 var express = require("express");
-var request = require("request");
+var proxy = require("http-proxy-middleware");
 var bodyParser = require("body-parser");
+
+// common variables
+const host = "localhost";
+const port = 3001;
+const meshHost = "localhost";
+const meshPort = 8080;
+const meshURL = "/api/v2/*";
+// -----------------------------------------------------------------------------------------------
+
+// Configure logs resinding
+
 const logzioLogger = require("logzio-nodejs").createLogger({
   token: "xMmDBYSIUNCxOWbhHstIlamPvOKJMUAB",
   host: "listener-eu.logz.io",
@@ -11,11 +22,43 @@ function sendLogToLogzIO(data) {
   logzioLogger.log(data);
 }
 
+// -----------------------------------------------------------------------------------------------
+
+// Create and configure proxy
+
+var options = {
+  target: `http://${host}:${port}`, // target host
+  ws: true, // proxy websockets
+  router: {
+    "localhost:3001": `http://${meshHost}:${meshPort}`
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*"; // add new header to response
+    if (!proxyRes.statusCode) {
+      // if proxy response doesn't have status code, set it up to OK (200)
+      proxyRes.statusCode = 200;
+    }
+  }
+};
+
+var filter = function(pathname, req) {
+  return req.method === "GET" || req.method === "PUT" || req.method === "POST";
+};
+
+var meshProxy = proxy(filter, options);
+
+// -----------------------------------------------------------------------------------------------
+
+// Configure server
+
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
+app.use(`${meshURL}`, meshProxy);
+
+// -----------------------------------------------------------------------------------------------
 
 app.post("/logger", function(request, response) {
   console.log(request.body);
@@ -24,22 +67,16 @@ app.post("/logger", function(request, response) {
   response.end("Logged");
 });
 
-app.get("/api/v2/auth/login", function(req, res) {
-  console.log(`GET from UI with URL: ${req.url}`);
-  console.log(req.body);
-
-  // var options = {
-  //   hostname: "localhost",
-  //   port: 8080,
-  //   path: "/api/v2/auth/login",
-  //   method: "GET",
-  //   headers: { authorization: "Basic ZGVtby51c2VyQG1haWwucnU6ZGVtbzEyMw==" }
-  // };
-  
-  res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-  req.pipe(request("http://localhost:8080" + req.url)).pipe(res);
+app.options(`${meshURL}`, function(request, response) {
+  response.writeHead(200, {
+    "Access-Control-Allow-Headers": ["Content-Type", "Accept", "Access-Control-Allow-Origin", "Authorization", "authorization"],
+    "Access-Control-Allow-Methods": ["OPTIONS", "HEAD", "DELETE", "POST", "PUT", "GET"],
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Expose-Headers": "",
+    "Allow": ["OPTIONS", "HEAD", "DELETE", "POST", "PUT", "GET"]
+  });
+  response.end("Accepted options");
 });
 
-port = 3001;
 app.listen(port);
-console.log(`Listening at http://localhost:${port}`);
+console.log(`Listening at http://${host}:${port}`);
