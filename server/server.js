bd02e40cf57a5b1df0e40ee0b8b6da5c9cc32e8f
komadmi <dmitriy.comarov@gmail.com>
2019-10-28 20:43:06 +0300

    Create user (small changes)

--- server/server.js
+++ server/server.js
@@ -1,13 +1,50 @@
-var express = require("express");
-var proxy = require("http-proxy-middleware");
-var bodyParser = require("body-parser");
+const express = require("express");
+const proxy = require("http-proxy-middleware");
+const bodyParser = require("body-parser");
 
 // common variables
 const host = "localhost";
 const port = 3001;
+
 const meshHost = "localhost";
 const meshPort = 8080;
-const meshURL = "/api/v2/*";
+const meshPrefix = "/api/v2";
+const meshURLAll = meshPrefix + "/*";
+const meshURLLogin = meshPrefix + "/auth/login";
+const meshURLUsers = meshPrefix + "/users";
+// -----------------------------------------------------------------------------------------------
+
+// Login under 'art_creator' user, store it's credentials and add them for all create user requests
+async function getAuthHeaderForArtCreatorUser() {
+  const headers = new Headers();
+  headers.append("Authorization", "Basic " + btoa("art_creator:art_creator123#"));
+  const response = await fetch(
+    new Request(`http://${meshHost}:${meshPort}${meshURLLogin}`, {
+      method: "GET",
+      headers: headers
+    })
+  );
+
+  if (!response.ok) {
+    throw new Error("Network response was not OK");
+  }
+  const jsonResponse = await response.json();
+  return {
+    name: "Authorization",
+    value: `Bearer ${jsonResponse.body.token}`
+  };
+}
+
+let createUserHeader = undefined;
+try {
+  createUserHeader = getAuthHeaderForArtCreatorUser();
+  console.log("'art_creator' user has been logged in successfully");
+} catch (e) {
+  console.error(
+    "Cannot get token for 'art_creator' user. Unauthorized clients will not able to create user",
+    e
+  );
+}
 // -----------------------------------------------------------------------------------------------
 
 // Configure logs resinding
@@ -38,6 +75,15 @@ var options = {
       // if proxy response doesn't have status code, set it up to OK (200)
       proxyRes.statusCode = 200;
     }
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (
+      createUserHeader &&
+      req.method === "POST" &&
+      req.url === `http://${host}:${port}${meshURLUsers}`
+    ) {
+      proxyReq.setHeader(createUserHeader.name, createUserHeader.value);
+    }
   }
 };
 
@@ -56,9 +102,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
-app.use(`${meshURL}`, meshProxy);
-
-// Login under 'art_creator' user, store it's credentials and add them for all create user requests
+app.use(`${meshURLAll}`, meshProxy);
 
 // -----------------------------------------------------------------------------------------------
 
@@ -69,10 +113,10 @@ app.post("/logger", function(request, response) {
   response.end("Logged");
 });
 
-app.options(`${meshURL}`, function(request, response) {
+app.options(`${meshURLAll}`, function(request, response) {
   response.writeHead(200, {
     "Access-Control-Allow-Headers": [
-      "Content-Type"
+      "Content-Type",
       "Accept",
       "Access-Control-Allow-Origin",
       "Authorization"
