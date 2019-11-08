c95fb52074102d7d6f3890def2f33c240238a971
komadmi <dmitriy.comarov@gmail.com>
2019-11-08 21:12:05 +0300

    NavBar and update dependencies

--- ui-web-react/src/components/header/header.component.tsx
+++ ui-web-react/src/components/header/header.component.tsx
@@ -2,40 +2,68 @@ import React from "react";
 import { UserInfo } from "../../model/types/datatypes";
 import Optional from "optional-js";
 import { DEFAULT, USER_SIGN_IN, USER_PROFILE } from "../../model/routing/paths";
-import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+// @ts-ignore
+import { useHistory } from "react-router-dom";
+import { getRoleName } from "../../model/types/roles.model";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export type HeaderComponentProps = {
   userInfo: Optional<UserInfo>;
 };
 
-export class HeaderComponent extends React.Component<HeaderComponentProps> {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" exact to={DEFAULT}>
-                Домой <span className="sr-only">(current)</span>
-              </NavLink>
-            </li>
-          </ul>
-          <div className="navbar-nav justify-content-end">
-            {this.props.userInfo
-              .map(info => this.renderUserLink("Привет, " + info.name, USER_PROFILE))
-              .orElse(this.renderUserLink("Войти", USER_SIGN_IN))}
-          </div>
-        </div>
-      </nav>
-    );
-  }
+const HeaderComponent: React.FC<HeaderComponentProps> = (props: HeaderComponentProps) => {
+  const classes = useStyles();
+  const history = useHistory();
+  const reRouteTo = (path: string): void => history.push(path);
 
-  renderUserLink = (label: string, path: string): JSX.Element => {
-    return (
-      <NavLink className="nav-item nav-link" exact to={path}>
-        {label + " "}
-        <span className="sr-only">(current)</span>
-      </NavLink>
-    );
-  };
-}
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Button color="inherit" onClick={() => reRouteTo(DEFAULT)}>
+              Домой
+            </Button>
+          </Typography>
+          {props.userInfo
+            .map(info => (
+              <Tooltip title={`${info.name} (${getRoleName(info.role)}, ${info.email})`}>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                  onClick={() => reRouteTo(USER_PROFILE)}
+                >
+                  <AccountCircle />
+                </IconButton>
+              </Tooltip>
+            ))
+            .orElse(
+              <Button color="inherit" onClick={() => reRouteTo(USER_SIGN_IN)}>
+                Войти
+              </Button>
+            )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default HeaderComponent;
