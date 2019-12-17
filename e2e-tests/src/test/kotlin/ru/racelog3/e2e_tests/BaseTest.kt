02afba7af0601ca1420ed8a1d394523daafa1ae9
komadmi <dmitriy.comarov@gmail.com>
2019-12-17 20:59:59 +0300

    initial

---
+++ e2e-tests/src/test/kotlin/ru/racelog3/e2e_tests/BaseTest.kt
@@ -0,0 +1,24 @@
+package ru.racelog3.e2e_tests
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfNestedElementLocatedBy;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+abstract class BaseTest {
+	
+}
